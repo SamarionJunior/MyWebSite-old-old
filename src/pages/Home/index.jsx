aa9adaf3c4e31752e4657db7f0f7956252db08bf
Samarión Júnior <samarion2@hotmail.com>
2023-05-19 15:17:25 -0300

    Corrigindo Linke e Funcionalidades que Tinha Quebrado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,56 +1,52 @@
-import React from 'react';
 import { Link } from "react-router-dom";
 import Style from './home.module.css';
+
+import axios from "axios";
+
 // import {createBrowserHistory} from "history";
 
 const Home = _ => {
-    // document.getElementById("ButtonCopyEmail").addEventListener("click", function copiarTexto() {
-    //     let textoCopiado = document.getElementById("TextCopyEmail");
-    //     console.log(textoCopiado)
-    //     textoCopiado.select();
-    //     textoCopiado.setSelectionRange(0, 99999)
-    //     document.execCommand("copy");
-    //     document.getElementById("name").focus()
-    // })
 
-    // document.getElementById("Submit").addEventListener("submit", e => {
-    //     e.preventDefault()
-    //     const SubmitAssunto = String(document.getElementById("_subject").value)
-    //     const SubmitNome = String(document.getElementById("name").value)
-    //     const SubmitEmail = String(document.getElementById("email").value)
-    //     const SubmitMessagem = String(document.getElementById("message").value)
+    const onSubmit = e => {
 
-    //     console.log(SubmitAssunto)
-    //     console.log(SubmitNome)
-    //     console.log(SubmitEmail)
-    //     console.log(SubmitMessagem)
+        e.preventDefault()
 
-    //     document.getElementById("Status").style.display = "block";
-    //     document.getElementById("Status").style.backgroundColor = "gray";
-    //     document.getElementById("Status").style.color = "white";
-    //     document.getElementById("Status").innerText = "Enviando Mensagem...";
+        const SubmitAssunto = String(document.getElementById("_subject").value)
+        const SubmitNome = String(document.getElementById("name").value)
+        const SubmitEmail = String(document.getElementById("email").value)
+        const SubmitMessagem = String(document.getElementById("message").value)
 
-        // axios.defaults.headers.post['Content-Type'] = 'application/json';
-        // axios.post('https://formsubmit.co/ajax/samarion2@hotmail.com', JSON.stringify({
-        //     _subject: SubmitAssunto,
-        //     name: SubmitNome,
-        //     email: SubmitEmail,
-        //     message: SubmitMessagem
-        // }))
-        // .then(response => {
-        //     console.log(response)
-        //     if(response.status === 200){
-        //         document.getElementById("Status").style.backgroundColor = "#339900";
-        //         document.getElementById("Status").innerText = "Seu E-mail Foi Enviado Com Sucesso!!!";
-        //     }
-        // })
-        // .catch(error => {
-        // console.log(error)
-        // document.getElementById("Status").style.backgroundColor = "#ff0f0f";
-        // document.getElementById("Status").innerText = "Houve um erro no envio do seu E-mail!!!"
-        // });
+        console.log(SubmitAssunto)
+        console.log(SubmitNome)
+        console.log(SubmitEmail)
+        console.log(SubmitMessagem)
 
-    // })
+        document.getElementById(`${Style.Status}`).style.display = "block";
+        document.getElementById(`${Style.Status}`).style.backgroundColor = "gray";
+        document.getElementById(`${Style.Status}`).style.color = "white";
+        document.getElementById(`${Style.Status}`).innerText = "Enviando Mensagem...";
+
+        axios.defaults.headers.post['Content-Type'] = 'application/json';
+        axios.post('https://formsubmit.co/ajax/samarionrtwertertwet2@hotmail.com', JSON.stringify({
+            _subject: SubmitAssunto,
+            name: SubmitNome,
+            email: SubmitEmail,
+            message: SubmitMessagem
+        }))
+        .then(response => {
+            console.log(response)
+            if(response.status === 200){
+                document.getElementById(`${Style.Status}`).style.backgroundColor = "#339900";
+                document.getElementById(`${Style.Status}`).innerText = "Seu E-mail Foi Enviado Com Sucesso!!!";
+            }
+        })
+        .catch(error => {
+        console.log(error)
+        document.getElementById(`${Style.Status}`).style.backgroundColor = "#ff0f0f";
+        document.getElementById(`${Style.Status}`).innerText = "Houve um erro no envio do seu E-mail!!!"
+        });
+
+    }
     
     function toLink(e, id) {
         e.preventDefault();
@@ -61,6 +57,15 @@ const Home = _ => {
             top: offsetTop,
             behavior: "smooth"
         });
+    }
+    function copyText() {
+        // console.log(`#${Style.TextCopyEmail}`)
+        let textoCopiado = document.getElementById("TextCopyEmail");
+        // console.log(textoCopiado)
+        textoCopiado.select();
+        textoCopiado.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        textoCopiado.setSelectionRange(0, 0)
     }
     return (
         <div className={Style.App}>
@@ -104,9 +109,9 @@ const Home = _ => {
                             Como por exemplo uma lista de compras.
                         </p>
                         <div className={Style.Actions}>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Web</Link>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Git</Link>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Apk</Link>
+                            <Link to="https://samarionjunior.github.io/ToDo/" className={Style.Action} target="_blank" rel="noopener noreferrer">Web</Link>
+                            <Link to="https://github.com/SamarionJunior/ToDo" className={Style.Action} target="_blank" rel="noopener noreferrer">Git</Link>
+                            <Link to="https://samarionjunior.github.io/ToDo/" className={Style.Action} target="_blank" rel="noopener noreferrer">Apk</Link>
                         </div>
                     </div>
                 </div>
@@ -121,9 +126,9 @@ const Home = _ => {
                             é uma enciclopédia virtual que detém todas as espécies de pokémon.
                         </p>
                         <div className={Style.Actions}>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Web</Link>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Git</Link>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Apk</Link>
+                            <Link to="https://samarionjunior.github.io/MyWebSite/" className={Style.Action} target="_blank" rel="noopener noreferrer">Web</Link>
+                            <Link to="https://samarionjunior.github.io/MyWebSite/" className={Style.Action} target="_blank" rel="noopener noreferrer">Git</Link>
+                            <Link to="https://samarionjunior.github.io/MyWebSite/" className={Style.Action} target="_blank" rel="noopener noreferrer">Apk</Link>
                         </div>
                     </div>
                 </div>
@@ -140,9 +145,9 @@ const Home = _ => {
                             o trabalho em períodos de 25 minutos, separados por breves intervalos. 
                         </p>
                         <div className={Style.Actions}>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Web</Link>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Git</Link>
-                            <Link to="/Home" className={Style.Action} target="_blank" rel="noopener noreferrer">Apk</Link>
+                            <Link to="https://samarionjunior.github.io/MyWebSite/" className={Style.Action} target="_blank" rel="noopener noreferrer">Web</Link>
+                            <Link to="https://samarionjunior.github.io/MyWebSite/" className={Style.Action} target="_blank" rel="noopener noreferrer">Git</Link>
+                            <Link to="https://samarionjunior.github.io/MyWebSite/" className={Style.Action} target="_blank" rel="noopener noreferrer">Apk</Link>
                         </div>
                     </div>
                 </div>
@@ -151,24 +156,24 @@ const Home = _ => {
             <div className={Style.View} id={Style.Contact}>
                 <div className={Style.AutoFill}>
                     <div className={Style.Information}>
-                        <form className={Style.Form} id={Style.Submit}>
+                        <form className={Style.Form} id={Style.Submit} onSubmit={e => onSubmit(e)}>
                             <span id={Style.Status}></span>
                             <input type="hidden" name="_captcha" value="false"/>
                             <div className={Style.Groups}>
                                 <label className={Style.LabelInputText} htmlFor="name">Nome: </label>
-                                <input className={Style.InputText} name="name" id={Style.name} type="text" placeholder="Digite seu Nome."/>
+                                <input className={Style.InputText} name="name" id="name" type="text" placeholder="Digite seu Nome."/>
                             </div>
                             <div className={Style.Groups}>
                                 <label className={Style.LabelInputText} htmlFor="_subject">Assunto: </label>
-                                <input className={Style.InputText} name="_subject" id={Style._subject} type="text" placeholder="Digite o Tema."/>
+                                <input className={Style.InputText} name="_subject" id="_subject" type="text" placeholder="Digite o Tema."/>
                             </div>
                             <div className={Style.Groups}>
                                 <label className={Style.LabelInputText} htmlFor="email">De: </label>
-                                <input className={Style.InputText} name="email" id={Style.email} type="text" placeholder="Digite seu e-mail."/>
+                                <input className={Style.InputText} name="email" id="email" type="text" placeholder="Digite seu e-mail."/>
                             </div>
                             <div className={Style.Groups}>
                                 <label className={Style.LabelInputText} htmlFor="message">Conteúdo:</label>
-                                <textarea className={Style.TextArea} name="message" id={Style.message} placeholder="Digite a Messagem."></textarea>
+                                <textarea className={Style.TextArea} name="message" id="message" placeholder="Digite a Messagem."></textarea>
                             </div>
                             <div className={Style.Groups}>
                                 <input className={`${Style.Button100} ${Style.ButtonDark} ${Style.InputButton}`} value="Enviar" id="submit" type="submit" name="submit"/>
@@ -176,16 +181,16 @@ const Home = _ => {
                         </form>
                         <div className={`${Style.Column} ${Style.Contact}`}>
                             <div className={`${Style.Row} ${Style.Email}`}>
-                                <input className={`${Style.Row} ${Style.EmailText}`} id={Style.TextCopyEmail} type="text" value="example2@e-mail.com" readOnly/>
-                                <i className="fa-regular fa-copy Row" id={Style.ButtonCopyEmail}></i>
+                                <input className={`${Style.Row} ${Style.EmailText}`} id="TextCopyEmail" type="text" value="samarion2@hotmail.com" readOnly/>
+                                <i className="fa-regular fa-copy Row" id={Style.ButtonCopyEmail}  onClick={(e) => copyText()}></i>
                             </div>
                         </div>
                         <div className={`${Style.Column} ${Style.SocialMidias}`}>
                             <div className={`${Style.Row} ${Style.Icons}`}>
-                                <Link to="https://www.github.com" target="_blank" rel="noopener noreferrer">
+                                <Link to="https://github.com/SamarionJunior" target="_blank" rel="noopener noreferrer">
                                     <i className="fa-brands fa-github"></i>
                                 </Link>
-                                <Link to="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                                <Link to="www.linkedin.com/in/samarion-junior" target="_blank" rel="noopener noreferrer">
                                     <i className="fa-brands fa-linkedin"></i>
                                 </Link>
                             </div>
